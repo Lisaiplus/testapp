f1f8f4652efb7feb043dad9feac3a5da193716b7
Lisaiplus <100062430+Lisaiplus@users.noreply.github.com>
2024-04-18 12:26:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
-base_path = './internlm2-chat-7b'
+base_path = './internlm2-chat-1_8B'
 os.system(f'git clone https://code.openxlab.org.cn/mzlx/testmodel.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16)
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float)
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.7,temperature=1):
         yield response
 
 gr.ChatInterface(chat,
-                 title="InternLM2-Chat-7B",
+                 title="InternLM2-Chat-1.8B",
                 description="""
 InternLM is mainly developed by Shanghai AI Laboratory.  
                  """,
