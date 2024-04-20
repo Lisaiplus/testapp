a912d92caae8d98158d98d58935168582c950696
Lisaiplus <100062430+Lisaiplus@users.noreply.github.com>
2024-04-20 21:20:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, AutoModel
 
 # download internlm2 to the base_path directory using git tool
 base_path = './internlm2-chat-1_8B'
-os.system(f'git clone https://code.openxlab.org.cn/mzlx/testmodel.git {base_path}')
+os.system(f'git clone https://code.openxlab.org.cn/mzlx/1.8b-4bit.git {base_path}')
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
